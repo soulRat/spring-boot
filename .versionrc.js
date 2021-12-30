192fdeceda99678528439e568c96d299143d4cd8
zhujx <ershu.zhujx@menggejk.com>
2021-12-30 11:40:00 +0800

    feat: 版本提交代码集成

--- .versionrc.js
+++ .versionrc.js
@@ -1,12 +1,32 @@
 // .versionrc.js
-const xmlUpdater = require('standard-version-updater-pom');
+const xmlUpdater = require('./index');
 
-const xmlTracker = {
+const xmlTracker1 = {
     filename: 'pom.xml',
     updater: xmlUpdater,
 };
 
+const xmlTracker2 = {
+    filename: 'spring-web/pom.xml',
+    updater: xmlUpdater,
+};
+
+const xmlTracker3 = {
+    filename: 'spring-biz/pom.xml',
+    updater: xmlUpdater,
+};
+
+const xmlTracker4 = {
+    filename: 'spring-common/pom.xml',
+    updater: xmlUpdater,
+};
+
+const xmlTracker5 = {
+    filename: 'spring-dal/pom.xml',
+    updater: xmlUpdater,
+};
+
 module.exports = {
-    packageFiles: [xmlTracker],
-    bumpFiles: [xmlTracker],
+    packageFiles: [xmlTracker1, xmlTracker2, xmlTracker3, xmlTracker4, xmlTracker5],
+    bumpFiles: [xmlTracker1, xmlTracker2, xmlTracker3, xmlTracker4, xmlTracker5],
 };
